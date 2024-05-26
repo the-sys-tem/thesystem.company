65acc8be8ca2686bce6013dce04aacc361123da7
Rudra Saraswat <rs2009@ubuntu.com>
2024-05-26 09:36:23 +0000

    feat: adapt website to thesystem

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://yoursite.com/",
+  site: "https://thesystem.company",
   integrations: [tailwind(), sitemap()]
 });
